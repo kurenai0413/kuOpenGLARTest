6bdb0ddb1fc1199e2b50f8efd9ea65d9adb6e583
kurenai0413_home <kurenai0413@hotmail.com>
2016-04-27 23:38:39 +0800

    Add codes for estimating extrinsic parameters. Codes of camera capture are commented out, simply using single image for tesing.

--- kuOpenGLARTest/kuOpenGLARTest.cpp
+++ kuOpenGLARTest/kuOpenGLARTest.cpp
@@ -6,11 +6,34 @@
 
 #pragma comment(lib,"opencv_world310d.lib")
 
-cv::Mat					frame;
-cv::VideoCapture	*	cap = NULL;
+using namespace cv;
+using namespace std;
+
+Mat					FrameRaw;
+Mat					frame;
+Mat					GrayImg;
+VideoCapture	*	cap = NULL;
+
+Mat					IntParam;
+Mat					DistParam;
+vector<Point3f>		CB3DPts;
+vector<Point2f>		CB2DPts;
+vector<Point2f>		Projected2DPts;
+Mat					RotationVec;
+Mat					RotationMat;
+Mat					TranslationVec;
+
+float				fovx;
+float				fovy;
+float				aspect;
 
 void Init();
 void DispFunc();
+void DispParam();
+void DispExtParam();
+void SetCB3DPts();
+bool LoadCameraParameters(char * Filename);
+void SaveExtrinsicParameters(char * Filename);
 void DrawAxes(float length);
 //void myKeyboard(unsigned char key, int mouseX, int mouseY);
 //void myReshape(int width, int height);
@@ -38,6 +61,28 @@ void Init()
 
 	// initialize OpenCV video capture
 	cap = new cv::VideoCapture(0);
+
+	IntParam.create(3, 3, CV_32FC1);
+	DistParam.create(1, 4, CV_32FC1);
+	RotationVec.create(3, 1, CV_64FC1);
+	RotationMat.create(3, 3, CV_64FC1);
+	TranslationVec.create(3, 1, CV_64FC1);
+
+	frame = imread("CamFrame.bmp", 1);
+
+	if (LoadCameraParameters("IntParam_Left.txt"))
+	{
+		float fx = IntParam.at<float>(0, 0);
+		float fy = IntParam.at<float>(1, 1);
+
+		fovx = 2 * atan(0.5 * 640 / fx) * 180 / 3.1415926;
+		fovy = 2 * atan(0.5 * 480 / fy) * 180 / 3.1415926;
+		aspect = (640 * fy) / (480 * fx);
+
+		DispParam();
+	}
+
+	SetCB3DPts();
 }
 
 void DispFunc()
@@ -45,11 +90,49 @@ void DispFunc()
 	// clear the window
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	cap->read(frame);				// capture image
-
+	//cap->read(frame);				// capture image
+	
 	// flip camera frame
-	cv::Mat tempimage;
-	cv::flip(frame, tempimage, 0);
+	Mat tempimage;
+	flip(frame, tempimage, 0);
+
+	cvtColor(tempimage, GrayImg, CV_RGB2GRAY);
+
+	bool CBFound = findChessboardCorners(GrayImg, Size(5, 7), CB2DPts,
+										 CALIB_CB_ADAPTIVE_THRESH + CALIB_CB_NORMALIZE_IMAGE);
+	drawChessboardCorners(tempimage, Size(5, 7), Mat(CB2DPts), CBFound);
+
+	if (CBFound)
+	{
+		//imwrite("CamFrame.bmp", FrameRaw);
+
+		solvePnP(CB3DPts, CB2DPts, IntParam, DistParam, RotationVec, TranslationVec);
+		Rodrigues(RotationVec, RotationMat);
+
+		vector<Point3f> Test3DPts;
+		vector<Point2f> Projected2DPts;
+
+		Test3DPts.resize(4);
+		Test3DPts[0] = Point3f(12.5, 0, 0);
+		Test3DPts[1] = Point3f(37.5, 25, 0);
+		Test3DPts[2] = Point3f(37.5, 75, 0);
+		Test3DPts[3] = Point3f(-37.5, 100, 0);
+
+		projectPoints(Test3DPts,
+			RotationVec, TranslationVec,
+			IntParam, DistParam, Projected2DPts);
+
+		for (int i = 0; i < 4; i++)
+		{
+			circle(tempimage, Projected2DPts[i], 1, CV_RGB(255, 0, 0), 5, CV_AA);
+		}
+
+		Test3DPts.clear();
+
+		DispExtParam();
+		SaveExtrinsicParameters("ExtParam_Left.txt");
+	}
+
 	glDrawPixels(tempimage.size().width, tempimage.size().height, GL_BGR_EXT, GL_UNSIGNED_BYTE, tempimage.ptr());
 
 	// set viewport
@@ -113,6 +196,90 @@ void DrawAxes(float length)
 	glEnd();
 
 	glPopAttrib();
+}
+
+void SetCB3DPts()
+{
+	for (int i = 0; i < 7; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			CB3DPts.push_back(Point3f(50 - 25 * j, 25 * i, 0));
+		}
+	}
+}
+
+void DispParam()
+{
+	for (int i = 0; i < 3; i++)
+	{
+		printf("%f %f %f\n", IntParam.at<float>(i, 0), IntParam.at<float>(i, 1),
+			IntParam.at<float>(i, 2));
+	}
+	printf("%f %f %f %f\n", DistParam.at<float>(0, 0), DistParam.at<float>(0, 1),
+		DistParam.at<float>(0, 2), DistParam.at<float>(0, 3));
+}
+
+void DispExtParam()
+{
+	for (int i = 0; i < 3; i++)
+	{
+		cout << RotationMat.at<double>(i, 0) << " "
+			<< RotationMat.at<double>(i, 1) << " "
+			<< RotationMat.at<double>(i, 2) << endl;
+	}
+
+	cout << endl;
+	cout << TranslationVec.at<double>(0, 0) << " "
+		<< TranslationVec.at<double>(1, 0) << " "
+		<< TranslationVec.at<double>(2, 0) << endl;
+	cout << endl;
+}
+
+bool LoadCameraParameters(char * Filename)
+{
+	FILE	*	fp;
+	errno_t		err;
+
+	err = fopen_s(&fp, Filename, "r");
+	if (err != 0)
+	{
+		return false;
+	}
+	else
+	{
+		for (int i = 0; i < 3; i++)
+		{
+			fscanf_s(fp, "%f %f %f\n", &IntParam.at<float>(i, 0),
+				&IntParam.at<float>(i, 1),
+				&IntParam.at<float>(i, 2));
+
+
+		}
+		fscanf_s(fp, "%f %f %f %f\n", &DistParam.at<float>(0, 0),
+			&DistParam.at<float>(0, 1),
+			&DistParam.at<float>(0, 2),
+			&DistParam.at<float>(0, 3));
+		fclose(fp);
+
+		return true;
+	}
+}
+
+void SaveExtrinsicParameters(char * Filename)
+{
+	FILE * fp;
+
+	errno_t err = fopen_s(&fp, Filename, "w");
+	for (int i = 0; i < 3; i++)
+	{
+		fprintf(fp, "%f %f %f %f\n", RotationMat.at<double>(i, 0),
+			RotationMat.at<double>(i, 1),
+			RotationMat.at<double>(i, 2),
+			TranslationVec.at<double>(i, 0));
+	}
+	fprintf(fp, "0.0 0.0 0.0 1.0");
+	fclose(fp);
 }
 
 //void myKeyboard(unsigned char key, int mouseX, int mouseY)
